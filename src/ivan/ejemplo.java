1922af77f11d362c3b5247174f0e7b024811acd6
ivan0027 <ivan.krasimirov@educa.madrid.org>
2024-05-09 18:31:05 +0200

    ejemplo

--- src/ivan/ejemplo.java
+++ src/ivan/ejemplo.java
@@ -4,5 +4,6 @@ public class ejemplo {
     public static void main(String[] args) {
         System.out.println("Practica entornos");
         System.out.println("desde github");
+        System.out.println("Punto 5");
     }
 }
