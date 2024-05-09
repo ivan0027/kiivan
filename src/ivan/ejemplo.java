70c4aabb84a382fa36c05198adbbee99ad4e128c
ivan0027 <167172740+ivan0027@users.noreply.github.com>
2024-05-09 18:52:06 +0200

    Update ejemplo.java

--- src/ivan/ejemplo.java
+++ src/ivan/ejemplo.java
@@ -5,6 +5,7 @@ public class ejemplo {
         System.out.println("Practica entornos");
         System.out.println("desde github");
         System.out.println("Punto 5");
-        System.out.println("Punto 6")
+        System.out.println("Punto 6");
+        System.out.println("ejemplo");
     }
 }
